defc0900184eabdc2deb538beb25d96ce0a9eca4
Shashi Kiran <kiranshashi14@hotmail.com>
2015-08-30 00:46:25 +0530

    digit divisible

---
+++ finddigits.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main() {
+	long N,dig,T,i,count,num;
+    	cin>>T;
+    	for(i=0;i<T;i++)
+    	{
+    		cin>>N;
+        	count=0;
+        	num=N;
+    		while(num)
+        	{
+        		dig=num%10;
+        		if(dig!=0 && N%dig==0)
+            		{
+            			count++;
+               		}
+        		num=num/10;
+        	}
+        	cout<<count<<endl;
+    	}
+    	return 0;
+}

d82be44775cb1f7d3dd862dcb168a2df16f16052
Shashi Kiran <kiranshashi14@hotmail.com>
2015-11-03 00:21:31 +0530

    finding count of substrings in given string

---
+++ substringcount.py
@@ -0,0 +1,10 @@
+str1=input()
+substr=input()
+count=0
+for i in range(len(str1)-len(substr)+1):
+    for j in range(len(substr)):
+        if str1[j+i]!=substr[j]:
+            break;
+    if j==len(substr)-1:
+        count=count+1
+print(count)
